f1abc7efd89243ff79cd1b2b46d3ff602e9428f6
Qingyao Lin <2650426778@qq.com>
2023-01-22 16:03:28 +0800

    初步实现相关 scanner 逻辑；但错误信息异常的不完善。将在其他的分支中完善报错信息。

--- src/com/qingyao/Main.java
+++ src/com/qingyao/Main.java
@@ -1,7 +1,11 @@
 package com.qingyao;
 
+import com.qingyao.lox.Lox;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello,world!");
+    public static void main(String[] args) throws IOException {
+        Lox.main(args);
     }
 }
---
+++ src/com/qingyao/lox/Lox.java
@@ -0,0 +1,77 @@
+package com.qingyao.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+    static boolean hadError = false;
+
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    // 读取文件执行
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        // Indicate an error in the exit code.
+        if (hadError) System.exit(65);
+    }
+
+
+    // 命令行交互式的输入与执行 REPL
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (; ; ) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+            hadError = false;
+        }
+    }
+
+
+    // 核心程序，执行 scanner、parsing 等
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        // For now, just print the tokens.
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    // 错误处理程序
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    // 报告错误的发生位置及相关信息
+    private static void report(int line, String where,
+                               String message) {
+
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+
+
+    }
+}
